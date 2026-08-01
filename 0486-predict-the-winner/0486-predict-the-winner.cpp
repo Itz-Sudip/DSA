cee832565d6f6a5099fe257cb9dfbbfd5e6777ea
Itz-Sudip <palsudipkumar3@gmail.com>
2026-08-01 15:45:54 +0530

    Time: 96 ms (16.74%), Space: 9.7 MB (69.71%) - LeetHub

--- 0486-predict-the-winner/0486-predict-the-winner.cpp
+++ 0486-predict-the-winner/0486-predict-the-winner.cpp
@@ -1,19 +1,14 @@
 class Solution {
-public:
-    bool f(vector<int> &nums,int l,int r,int p,long long s1,long long s2){
-        if(l>r){
-            return s1>=s2;
-        }
-        if(p){
-            if(f(nums,l+1,r,0,s1+nums[l],s2) || f(nums,l,r-1,0,s1+nums[r],s2)) return true;
-        }
-        if(!p){
-            if(f(nums,l+1,r,1,s1,s2+nums[l]) && f(nums,l,r-1,1,s1,s2+nums[r])) return true;
-        }
-        return false;
+private:
+    int f(vector<int> &nums,int l,int r){
+        if(l==r) return nums[l];
+        int left=nums[l]-f(nums,l+1,r);
+        int right=nums[r]-f(nums,l,r-1);
+        return max(left,right);
     }
+public:
     bool predictTheWinner(vector<int>& nums) {
         int i=0,j=nums.size()-1;
-        return f(nums,i,j,1,0LL,0LL);
+        return f(nums,i,j)>=0;
     }
 };
